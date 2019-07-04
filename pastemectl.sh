60da28b213b02f88538f9af7b0981e1070d1ee80
Lucien <lucien@lucien.ink>
2019-07-04 09:26:34 +0000

    build from travis-ci 2019-07-04 17:26:34

---
+++ pastemectl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ ${#} != 1 ]]; then
+    echo "Usage: ${0} <start|stop|restart|status|log>"
+else
+    if [[ ${1} == "start" || ${1} == "stop" || ${1} == "restart" ]]; then
+        systemctl ${1} pastemed
+    elif [[ ${1} == "status" ]]; then
+        systemctl status pastemed | grep Active
+    elif [[ ${1} == "log" ]]; then
+        journalctl -xe -u pastemed | cat
+    else
+        echo "[ERROR] unsupported operation: ${1}"
+        exit 1
+    fi
+fi
